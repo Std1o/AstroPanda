3e94bfb1b39ea88d75ced5a20e545952f7b026c3
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-29 22:15:19 +0300

    0.2.1

---
+++ app/src/main/java/com/stdio/astropanda/Q8Activity.java
@@ -0,0 +1,14 @@
+package com.stdio.astropanda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Q8Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q8);
+    }
+}
