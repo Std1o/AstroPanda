8b94a11989cb05ff5307a87d04c9d4329a39e66e
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-29 20:53:32 +0300

    0.1.4

---
+++ app/src/main/java/com/stdio/astropanda/Q2Activity.java
@@ -0,0 +1,14 @@
+package com.stdio.astropanda;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Q2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q2);
+    }
+}
