12d8fc064de23e920d673553bc7a80e83d358baf
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-29 21:24:34 +0300

    0.1.6

--- app/src/main/java/com/stdio/astropanda/Q3Activity.java
+++ app/src/main/java/com/stdio/astropanda/Q3Activity.java
@@ -2,13 +2,25 @@ package com.stdio.astropanda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class Q3Activity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q3);
+
+        progressBar = findViewById(R.id.progressId);
+        progressBar.setProgress(34);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, Q4Activity.class));
     }
 }
