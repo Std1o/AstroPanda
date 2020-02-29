e50edc57ef35b512e0a15af160041ec5f37eb66f
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-29 22:08:58 +0300

    0.2.0

--- app/src/main/java/com/stdio/astropanda/Q7Activity.java
+++ app/src/main/java/com/stdio/astropanda/Q7Activity.java
@@ -2,25 +2,19 @@ package com.stdio.astropanda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ProgressBar;
 
-public class Q6Activity extends AppCompatActivity {
+public class Q7Activity extends AppCompatActivity {
 
     private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_q6);
+        setContentView(R.layout.activity_q7);
 
         progressBar = findViewById(R.id.progressId);
-        progressBar.setProgress(67);
-    }
-
-    public void onClick(View view) {
-        startActivity(new Intent(this, Q7Activity.class));
+        progressBar.setProgress(78);
     }
 }
