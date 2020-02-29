83e936e623741748e282f0ccabfe9e4acd78160a
Std1o <37378410+Std1o@users.noreply.github.com>
2020-02-29 18:22:32 +0300

    0.0.8

--- app/src/main/java/com/stdio/astropanda/GetVipActivity.java
+++ app/src/main/java/com/stdio/astropanda/GetVipActivity.java
@@ -2,7 +2,9 @@ package com.stdio.astropanda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GetVipActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class GetVipActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_vip);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, Q1Activity.class));
     }
 }
