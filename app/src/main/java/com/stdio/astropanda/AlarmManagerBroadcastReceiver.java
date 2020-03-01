c8b4f2a8729620ee2ce5a8041d734c88739f1a72
Std1o <37378410+Std1o@users.noreply.github.com>
2020-03-01 14:45:04 +0300

    0.2.8

---
+++ app/src/main/java/com/stdio/astropanda/AlarmManagerBroadcastReceiver.java
@@ -0,0 +1,91 @@
+package com.stdio.astropanda;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+public class AlarmManagerBroadcastReceiver extends BroadcastReceiver {
+    final public static String ONE_TIME="onetime";
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        PowerManager pm=(PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wl= pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,"YOUR TAG");
+//Осуществляем блокировку
+        wl.acquire();
+        SharedPreferences prefs = context.getSharedPreferences("moneyPref", Context.MODE_PRIVATE);
+        if (prefs.getInt("moneyCount", 0) != 1000) {
+            sendNotification(context, context.getString(R.string.push));
+        }
+
+//Разблокируем поток.
+        wl.release();
+    }
+    public void SetAlarm(Context context)
+    {
+        AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent=new Intent(context, AlarmManagerBroadcastReceiver.class);
+        intent.putExtra(ONE_TIME, Boolean.FALSE);//Задаем параметр интента
+        PendingIntent pi= PendingIntent.getBroadcast(context,0, intent,0);
+//Устанавливаем интервал срабатывания в 5 секунд.
+        am.setRepeating(AlarmManager.RTC_WAKEUP,System.currentTimeMillis(),1000*60*60*24,pi);
+    }
+
+
+    public void sendNotification(Context context, String messageBody) {
+        int notificationCode = 378;
+
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, notificationCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri defaultRingtone = null;
+        defaultRingtone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel channel = new NotificationChannel("dayPlanner",
+                    "Channel name",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Channel description");
+            notificationManager.createNotificationChannel(channel);
+            NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(context, "dayPlanner")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setSound(defaultRingtone)
+                    .setContentTitle(context.getResources().getString(R.string.app_name))
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setContentText(messageBody);
+            notificationManager.notify(notificationCode, notificationCompat.build());
+        } else {
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationCompat.Builder notificationCompat = new NotificationCompat.Builder(context, "dayPlanner")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setSound(defaultRingtone)
+                    .setContentTitle(context.getResources().getString(R.string.app_name))
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setContentText(messageBody);
+            notificationManager.notify(notificationCode, notificationCompat.build());
+        }
+
+    }
+}
--- app/src/main/java/com/stdio/astropanda/MainActivity.java
+++ app/src/main/java/com/stdio/astropanda/MainActivity.java
@@ -2,16 +2,28 @@ package com.stdio.astropanda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AlarmManagerBroadcastReceiver alarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alarm=new AlarmManagerBroadcastReceiver();
+
+        Context context= this.getApplicationContext();
+        if(alarm!=null){
+            alarm.SetAlarm(context);
+        }else{
+            Toast.makeText(context,"Alarm is null", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void onClick(View view) {
