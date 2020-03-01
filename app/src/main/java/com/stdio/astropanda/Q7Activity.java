2cb34b4a0a2a7b99aab981ecebe499f279ba65d4
Std1o <37378410+Std1o@users.noreply.github.com>
2020-03-01 17:29:50 +0300

    Update Q7Activity.java

--- app/src/main/java/com/stdio/astropanda/Q7Activity.java
+++ app/src/main/java/com/stdio/astropanda/Q7Activity.java
@@ -3,24 +3,64 @@ package com.stdio.astropanda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Q7Activity extends AppCompatActivity {
 
     private ProgressBar progressBar;
+    EditText etAdvice;
+    TextView tvMoney;
+    SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q7);
 
+        SharedPreferences languagePref = getSharedPreferences("languagePref", MODE_PRIVATE);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale(GetVipActivity.getLanguageFromPosition(languagePref
+                .getInt("language", GetVipActivity.getSpinnerPosition(Locale.getDefault().getCountry())))));
+        res.updateConfiguration(conf, dm);
+        setContentView(R.layout.activity_q7);
+        etAdvice = findViewById(R.id.etAdvice);
+
+        SharedPreferences currentPagePref = getSharedPreferences("currentPagePref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = currentPagePref.edit();
+        editor.putString("currentPage", getClass().getSimpleName());
+        editor.apply();
+
         progressBar = findViewById(R.id.progressId);
         progressBar.setProgress(78);
+
+        tvMoney = findViewById(R.id.tvMoney);
+        prefs = getSharedPreferences("moneyPref", MODE_PRIVATE);
+        tvMoney.setText(prefs.getInt("moneyCount", 0) + "");
     }
 
     public void onClick(View view) {
-        startActivity(new Intent(this, Q8Activity.class));
+        if (!etAdvice.getText().toString().isEmpty()) {
+            MainActivity.message += "\n\nЧего вам не хватает в текущем способе учета личных финансов? - " + etAdvice.getText().toString();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("moneyCount", prefs.getInt("moneyCount", 0) + 200);
+            editor.apply();
+            startActivity(new Intent(this, Q8Activity.class));
+            finish();
+        }
+        else {
+            Toast.makeText(this, getResources().getString(R.string.empty_field), Toast.LENGTH_SHORT).show();
+        }
     }
 }
