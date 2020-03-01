ae93e209929e9012753331d409a149ef5530932d
Std1o <37378410+Std1o@users.noreply.github.com>
2020-03-01 13:28:55 +0300

    0.2.6

--- app/src/main/java/com/stdio/astropanda/CompleteActivity.java
+++ app/src/main/java/com/stdio/astropanda/CompleteActivity.java
@@ -2,7 +2,12 @@ package com.stdio.astropanda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
+import com.stdio.astropanda.slideShow.SlideActivity1;
 
 public class CompleteActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class CompleteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete);
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, SlideActivity1.class));
+        Animatoo.animateFade(this);
     }
 }
