64b2f251f92a0b32c532f32367ef9c4468ae0925
Std1o <37378410+Std1o@users.noreply.github.com>
2020-03-01 17:24:05 +0300

    Update Q6Activity.java

--- app/src/main/java/com/stdio/astropanda/Q6Activity.java
+++ app/src/main/java/com/stdio/astropanda/Q6Activity.java
@@ -3,24 +3,84 @@ package com.stdio.astropanda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class Q6Activity extends AppCompatActivity {
 
     private ProgressBar progressBar;
+    int id = 0;
+    String answer = "";
+    TextView tvMoney;
+    SharedPreferences prefs;
+    boolean nextIsAllowed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_q6);
 
+        SharedPreferences languagePref = getSharedPreferences("languagePref", MODE_PRIVATE);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale(GetVipActivity.getLanguageFromPosition(languagePref
+                .getInt("language", GetVipActivity.getSpinnerPosition(Locale.getDefault().getCountry())))));
+        res.updateConfiguration(conf, dm);
+        setContentView(R.layout.activity_q6);
+
+        tvMoney = findViewById(R.id.tvMoney);
+        prefs = getSharedPreferences("moneyPref", MODE_PRIVATE);
+        tvMoney.setText(prefs.getInt("moneyCount", 0) + "");
+
+        SharedPreferences currentPagePref = getSharedPreferences("currentPagePref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = currentPagePref.edit();
+        editor.putString("currentPage", getClass().getSimpleName());
+        editor.apply();
+
         progressBar = findViewById(R.id.progressId);
         progressBar.setProgress(67);
+
+        RadioButton redRadioButton = findViewById(R.id.radioButton1);
+        redRadioButton.setOnClickListener(radioButtonClickListener);
+
+        RadioButton greenRadioButton = findViewById(R.id.radioButton2);
+        greenRadioButton.setOnClickListener(radioButtonClickListener);
     }
 
+    View.OnClickListener radioButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            RadioButton rb = (RadioButton)v;
+            id = rb.getId();
+            answer = "" + rb.getText();
+            nextIsAllowed = true;
+        }
+    };
+
     public void onClick(View view) {
-        startActivity(new Intent(this, Q7Activity.class));
+        if (nextIsAllowed) {
+            MainActivity.message += "\n\nИнтересно ли вам стать частью астрологического/эзотерического сообщества,\n" +
+                    "        в котором будут: мастер-классы от разных спикеров, новые темы для изучения, дискуссионные\n" +
+                    "        площадки, общение с единомышленниками через чаты и тд? - ";
+            MainActivity.message += answer;
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putInt("moneyCount", prefs.getInt("moneyCount", 0) + 110);
+            editor.apply();
+            startActivity(new Intent(this, Q7Activity.class));
+            finish();
+        }
+        else {
+            Toast.makeText(this, getResources().getString(R.string.err_no_selected), Toast.LENGTH_SHORT).show();
+        }
     }
 }
