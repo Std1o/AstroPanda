b64896473d339050c214ac156a786e8aabfb3a18
Std1o <37378410+Std1o@users.noreply.github.com>
2020-03-03 23:55:40 +0300

    0.5.1

--- app/src/main/java/com/stdio/astropanda/Q6Activity.java
+++ app/src/main/java/com/stdio/astropanda/Q6Activity.java
@@ -1,18 +1,29 @@
 package com.stdio.astropanda;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatSpinner;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class Q6Activity extends AppCompatActivity {
@@ -23,20 +34,20 @@ public class Q6Activity extends AppCompatActivity {
     TextView tvMoney;
     SharedPreferences prefs;
     boolean nextIsAllowed = false;
+    TextView tvName, tvAge;
+    AppCompatSpinner spinner;
+    ArrayList<Integer> images = new ArrayList<>();
+    SharedPreferences languagePref;
+    boolean isShowed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_q6);
 
-        SharedPreferences languagePref = getSharedPreferences("languagePref", MODE_PRIVATE);
-        Resources res = getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        android.content.res.Configuration conf = res.getConfiguration();
-        conf.setLocale(new Locale(GetVipActivity.getLanguageFromPosition(languagePref
-                .getInt("language", GetVipActivity.getSpinnerPosition(Locale.getDefault().getCountry())))));
-        res.updateConfiguration(conf, dm);
+        setLanguage();
         setContentView(R.layout.activity_q6);
+        initAndSetViews();
+        initSpinner();
 
         tvMoney = findViewById(R.id.tvMoney);
         prefs = getSharedPreferences("moneyPref", MODE_PRIVATE);
@@ -55,6 +66,99 @@ public class Q6Activity extends AppCompatActivity {
 
         RadioButton greenRadioButton = findViewById(R.id.radioButton2);
         greenRadioButton.setOnClickListener(radioButtonClickListener);
+    }
+
+    private void setLanguage() {
+        languagePref = getSharedPreferences("languagePref", MODE_PRIVATE);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        android.content.res.Configuration conf = res.getConfiguration();
+        conf.setLocale(new Locale(GetVipActivity.getLanguageFromPosition(languagePref
+                .getInt("language", GetVipActivity.getSpinnerPosition(Locale.getDefault().getCountry())))));
+        res.updateConfiguration(conf, dm);
+    }
+
+    private void initAndSetViews() {
+        tvName = findViewById(R.id.tvName);
+        tvAge = findViewById(R.id.tvAge);
+
+        tvName.setText(new PrefManager(this).getName());
+        tvAge.setText(new PrefManager(this).getAge());
+    }
+
+    private void initSpinner() {
+        images.add(R.drawable.en);
+        images.add(R.drawable.de);
+        images.add(R.drawable.es);
+        images.add(R.drawable.fr);
+        images.add(R.drawable.it);
+        images.add(R.drawable.ru);
+
+        spinner = findViewById(R.id.spinner2);
+
+        ArrayAdapter<Integer> spinnerAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_dropdown_item, images) {
+
+            @Override
+            public boolean isEnabled(int position) {
+                return true;
+            }
+
+            @Override
+            public boolean areAllItemsEnabled() {
+                return false;
+            }
+
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View v = convertView;
+                if (v == null) {
+                    Context mContext = this.getContext();
+                    LayoutInflater vi = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    v = vi.inflate(R.layout.row2, null);
+                }
+
+                ImageView ivDrink = v.findViewById(R.id.imageView);
+                ivDrink.setImageDrawable(ResourcesCompat.getDrawable(getResources(), images.get(position), null));
+                return v;
+            }
+
+            @Override
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View v = convertView;
+                if (v == null) {
+                    Context mContext = this.getContext();
+                    LayoutInflater vi = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    v = vi.inflate(R.layout.row_drop_down, null);
+                }
+                ImageView ivDrink = v.findViewById(R.id.imageView);
+                ivDrink.setImageDrawable(ResourcesCompat.getDrawable(getResources(), images.get(position), null));
+                return v;
+            }
+        };
+
+        spinner.setAdapter(spinnerAdapter);
+        System.out.println(Locale.getDefault().getCountry());
+        int pos = languagePref.getInt("language", GetVipActivity.getSpinnerPosition(Locale.getDefault().getCountry()));
+        spinner.setSelection(pos);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                SharedPreferences.Editor editor = languagePref.edit();
+                editor.putInt("language", position);
+                editor.apply();
+                if (isShowed) {
+                    Toast.makeText(Q6Activity.this, getString(R.string.restart_for_changes), Toast.LENGTH_SHORT).show();
+                } else {
+                    isShowed = true;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     View.OnClickListener radioButtonClickListener = new View.OnClickListener() {
