1bf46f2e1b8ebcaac42d168cf41e117002e6c3b2
Std1o <37378410+Std1o@users.noreply.github.com>
2020-03-03 21:15:15 +0300

    0.4.5

--- app/src/main/java/com/stdio/astropanda/Q9Activity.java
+++ app/src/main/java/com/stdio/astropanda/Q9Activity.java
@@ -2,16 +2,20 @@ package com.stdio.astropanda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.stdio.astropanda.gmailHelper.GMailSender;
 
 import java.util.Locale;
 
@@ -21,6 +25,9 @@ public class Q9Activity extends AppCompatActivity {
     EditText etAdvice;
     TextView tvMoney;
     SharedPreferences prefs;
+    String recipient = "martiality.me@yandex.ru";
+    String senderMail = "finic.app@gmail.com";
+    String senderPassword = "yourpassword";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,5 +68,34 @@ public class Q9Activity extends AppCompatActivity {
         else {
             Toast.makeText(this, getResources().getString(R.string.empty_field), Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void sendMessage() {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setTitle("Sending Email");
+        dialog.setMessage("Please wait...");
+        dialog.setCancelable(false);
+        dialog.show();
+        Thread sender = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    GMailSender sender = new GMailSender(senderMail, senderPassword);
+                    sender.sendMail(getResources().getString(R.string.app_name), MainActivity.message,
+                            senderMail,
+                            recipient);
+                    dialog.dismiss();
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putInt("moneyCount", prefs.getInt("moneyCount", 0) + 200);
+                    editor.apply();
+                    startActivity(new Intent(Q9Activity.this, CompleteActivity.class));
+                    finish();
+                } catch (Exception e) {
+                    Log.e("mylog", "Error: " + e.getMessage());
+                }
+            }
+        });
+        sender.start();
     }
 }
